535d9a92b5520e736351b1472985c731957037f2
Wirt S. <34081557+Wirt4@users.noreply.github.com>
2026-03-13 22:59:48 -0500

    remove some superfluous work

--- app/controllers/console_controller.rb
+++ app/controllers/console_controller.rb
@@ -1,6 +1,7 @@
+# Public: Controller for the public-facing console page.
+#
+# Bypasses authentication so that any visitor can access the console
+# without needing to log in.
 class ConsoleController < ApplicationController
   allow_unauthenticated_access
-
-  def index
-  end
 end
