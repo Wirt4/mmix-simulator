703b421f3b3fe10ba37e3de4228fa7cd8f2f7308
Wirt S. <34081557+Wirt4@users.noreply.github.com>
2026-03-14 20:59:11 -0500

    repair editor import

--- app/javascript/controllers/editor_controller.js
+++ app/javascript/controllers/editor_controller.js
@@ -1,5 +1,5 @@
 import { Controller } from "@hotwired/stimulus"
-import Editor from "../editor"
+import Editor from "editor"
 
 export default class extends Controller {
   static targets = ["textarea", "lineNumbers"]
