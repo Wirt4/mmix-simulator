a12614b9d64326c39fe3de7ee27fbbb9e41fbc94
Wirt S. <34081557+Wirt4@users.noreply.github.com>
2026-03-17 10:47:14 -0500

    green

--- app/controllers/concerns/authorization.rb
+++ app/controllers/concerns/authorization.rb
@@ -1,1 +1,9 @@
-module Authorization end
+module Authorization
+  extend ActiveSupport::Concern
+
+  class_methods do
+    def require_role(*roles, **options)
+      before_action(**options) { }
+    end
+  end
+end
