48872819dc55197bf5743ff4b5250ec6edff4ea4
Wirt S. <34081557+Wirt4@users.noreply.github.com>
2026-03-17 10:52:29 -0500

    green

--- app/controllers/concerns/authorization.rb
+++ app/controllers/concerns/authorization.rb
@@ -3,7 +3,11 @@ module Authorization
 
   class_methods do
     def require_role(*roles, **options)
-      before_action(**options) { }
+      before_action(**options) do
+        unless roles.any? { |role| Current.user&.public_send(:"#{role}?") }
+          redirect_to root_path, alert: "Not authorized."
+        end
+      end
     end
   end
 end
