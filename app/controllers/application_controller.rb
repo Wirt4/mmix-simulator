e053355da7bd178a471cac1c43c46c26f979d7a0
Wirt S. <34081557+Wirt4@users.noreply.github.com>
2026-03-17 14:38:51 -0500

    green

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@
 # Includes authentication behavior and restricts access to modern browsers.
 # Automatically invalidates ETags when the importmap changes.
 class ApplicationController < ActionController::Base
-  include Authentication
+  include Authentication, Authorization
   allow_browser versions: :modern
   stale_when_importmap_changes
 end
