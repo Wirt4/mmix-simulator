159c7cfd08c93ac0189680b3cfe6e0db0090619e
Wirt S. <34081557+Wirt4@users.noreply.github.com>
2026-03-17 17:36:26 -0500

    red

--- test/requests/users_controller_test.rb
+++ test/requests/users_controller_test.rb
@@ -1,26 +1,36 @@
 require "test_helper"
 
-class UsersControllerTest <ActionDispatch::IntegrationTest
-setup do
+class UsersControllerTest < ActionDispatch::IntegrationTest
+  setup do
     @admin = User.create!(email_address: "admin@test.com",
                           user_name: "administrator",
                           password: "password",
                           role: "admin")
-@user = User.create!(email_address: "user@test.com",
-
-                          user_name: "average user",
-                          password: "password",
-                          role: "user")
+    @user = User.create!(email_address: "user@test.com",
+                         user_name: "average user",
+                         password: "password",
+                         role: "user")
   end
 
-test "should allow admin to access index" do
-  sign_in_as(@admin)
-  get users_url
-  assert_response :success
-end
-test "should not allow userto access index" do
-  sign_in_as(@user)
-  get users_url
-  assert_response :forbidden
-end
+  test "should allow admin to access index" do
+    sign_in_as(@admin)
+    get users_url
+    assert_response :success
+  end
+
+  test "should not allow user to access index" do
+    sign_in_as(@user)
+    get users_url
+    assert_response :forbidden
+  end
+
+  test "index: list all users with their roles" do
+    sign_in_as(@admin)
+    get users_url
+    assert_response :success
+    assert_select "td", text: @admin.user_name
+    assert_select "td", text: @user.user_name
+    assert_select "td", text: "user"
+    assert_select "td", text: "admin"
+  end
 end
