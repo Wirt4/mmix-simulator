3a7af4513a13269eab5596295b18befeb2657a83
Wirt S. <34081557+Wirt4@users.noreply.github.com>
2026-03-17 10:43:42 -0500

    authorization tests

---
+++ app/controllers/concerns/authorization.rb
@@ -0,0 +1,1 @@
+module Authorization end
---
+++ test/requests/concerns/authorization_test.rb
@@ -0,0 +1,109 @@
+require "test_helper"
+
+class AuthorizationTest < ActiveSupport::TestCase
+  class FakeController
+    include ActiveSupport::Callbacks
+    define_callbacks :action
+
+    class << self
+      attr_reader :before_actions
+
+      def before_action(**options, &block)
+        @before_actions ||= []
+        @before_actions << { options: options, block: block }
+      end
+    end
+
+    include Authorization
+
+    attr_accessor :redirected_to, :redirect_alert
+
+    def redirect_to(path, alert: nil)
+      @redirected_to = path
+      @redirect_alert = alert
+    end
+
+    def root_path
+      "/"
+    end
+  end
+
+  test "require_role registers a before_action" do
+    klass = Class.new(FakeController) do
+      require_role :admin
+    end
+
+    assert_equal 1, klass.before_actions.length
+  end
+
+  test "require_role passes options through to before_action" do
+    klass = Class.new(FakeController) do
+      require_role :admin, only: :destroy
+    end
+
+    assert_equal({ only: :destroy }, klass.before_actions.first[:options])
+  end
+
+  test "require_role redirects when user role is not in allowed roles" do
+    klass = Class.new(FakeController) do
+      require_role :admin
+    end
+
+    controller = klass.new
+    user = users(:one)
+    assert user.user?
+
+    Current.set(session: user.sessions.create!) do
+      controller.instance_exec(&klass.before_actions.first[:block])
+    end
+
+    assert_equal "/", controller.redirected_to
+    assert_equal "Not authorized.", controller.redirect_alert
+  end
+
+  test "require_role does not redirect when user role is in allowed roles" do
+    klass = Class.new(FakeController) do
+      require_role :admin
+    end
+
+    controller = klass.new
+    admin = users(:admin)
+    assert admin.admin?
+
+    Current.set(session: admin.sessions.create!) do
+      controller.instance_exec(&klass.before_actions.first[:block])
+    end
+
+    assert_nil controller.redirected_to
+  end
+
+  test "require_role accepts multiple roles" do
+    klass = Class.new(FakeController) do
+      require_role :user, :admin
+    end
+
+    controller = klass.new
+    user = users(:one)
+
+    Current.set(session: user.sessions.create!) do
+      controller.instance_exec(&klass.before_actions.first[:block])
+    end
+
+    assert_nil controller.redirected_to
+  end
+
+  test "require_role allows admin when multiple roles specified" do
+    klass = Class.new(FakeController) do
+      require_role :user, :admin
+    end
+
+    controller = klass.new
+    admin = users(:admin)
+
+    Current.set(session: admin.sessions.create!) do
+      controller.instance_exec(&klass.before_actions.first[:block])
+    end
+
+    assert_nil controller.redirected_to
+  end
+end
