b2b7956af0910126e3f4d63d1eaa9146c252022d
Wirt S. <34081557+Wirt4@users.noreply.github.com>
2026-03-18 09:30:51 -0500

    green

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -26,7 +26,7 @@ class SessionsController < ApplicationController
 
   def destroy
     terminate_session
-    redirect_to new_session_path
+    redirect_to new_session_path, status: :see_other
   end
 
   def logout
