5a5e53a8b35167cde4a1f2bf02074ea2fc77e507
Wirt S. <34081557+Wirt4@users.noreply.github.com>
2026-03-17 19:45:14 -0500

    green

--- test/requests/users_controller_test.rb
+++ test/requests/users_controller_test.rb
@@ -19,7 +19,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "index: list all users with their roles" do
- sign_in_as(@admin)
+    sign_in_as(@admin)
     get users_url
     assert_response :success
     assert_select "td", text: @admin.user_name
@@ -29,9 +29,15 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "index: users have edit option" do
-        sign_in_as(@admin)
+    sign_in_as(@admin)
     get users_url
     assert_response :success
     assert_select "button", text: "Edit", count: User.count
+  end
+
+  test "index: clicking edit redirects to edit page" do
+    sign_in_as(@admin)
+    get users_url
+    assert_select "a[href=?]", edit_user_path(@user), text: "Edit"
   end
 end
