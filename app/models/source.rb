ec7712229b1ba9267385b1a727ef878b4f159c13
Wirt S. <34081557+Wirt4@users.noreply.github.com>
2026-03-23 15:38:34 -0500

    Revert "model change"
    This reverts commit b00eaca834b59035c09f38bf7f87ed1cb602abb2.

--- app/models/source.rb
+++
@@ -1,6 +0,0 @@
-class Source < ApplicationRecord
-  belongs_to :user
-
-  validates :title, presence: true
-  validates :body, presence: true
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_23_203447) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_16_203500) do
   create_table "sessions", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "ip_address"
@@ -18,15 +18,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_23_203447) do
     t.string "user_agent"
     t.integer "user_id", null: false
     t.index ["user_id"], name: "index_sessions_on_user_id"
-  end
-
-  create_table "sources", force: :cascade do |t|
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.string "title"
-    t.datetime "updated_at", null: false
-    t.integer "user_id", null: false
-    t.index ["user_id"], name: "index_sources_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -41,5 +32,4 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_23_203447) do
   end
 
   add_foreign_key "sessions", "users"
-  add_foreign_key "sources", "users"
 end
