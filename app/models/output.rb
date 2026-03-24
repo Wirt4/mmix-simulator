05a7424422eb39973ad68904cd310cbb55d7811a
Wirt S. <34081557+Wirt4@users.noreply.github.com>
2026-03-23 23:21:37 -0500

    red

--- app/models/output.rb
+++ app/models/output.rb
@@ -1,3 +1,6 @@
+# Public: Represents the output produced by an MMIX simulation run.
+#
+# body - The String text content of the simulation output.
 class Output < ApplicationRecord
   validates :body, presence: true
 end
---
+++ test/models/executable_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class ExecutableTest < ActiveSupport::TestCase
+  test "Executable model exists" do
+    assert defined?(Executable), "Expected Executable model to be defined"
+  end
+end
