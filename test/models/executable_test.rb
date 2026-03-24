6ecc24ca3fd73116d72858aa64897ac8bb230c85
Wirt S. <34081557+Wirt4@users.noreply.github.com>
2026-03-24 12:43:33 -0500

    removed redundant test

--- test/models/executable_test.rb
+++ test/models/executable_test.rb
@@ -1,10 +1,6 @@
 require "test_helper"
 
 class ExecutableTest < ActiveSupport::TestCase
-  test "Executable model exists" do
-    assert defined?(Executable), "Expected Executable model to be defined"
-  end
-
   test "executable has many outputs" do
     assert_equal :has_many, Executable.reflect_on_association(:outputs).macro
   end
