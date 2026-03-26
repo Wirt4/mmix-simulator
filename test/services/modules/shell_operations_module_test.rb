e3da5b4d852d42cf2f7fe9ae6c9235759022c325
Wirt S. <34081557+Wirt4@users.noreply.github.com>
2026-03-26 08:55:53 -0500

    refactored test to reduce duplication

--- test/services/modules/shell_operations_module_test.rb
+++ test/services/modules/shell_operations_module_test.rb
@@ -9,25 +9,28 @@ class ShellOperationsModuleTest < ActiveSupport::TestCase
     @instance = TestClass.new
   end
 
+  def mockClass(output)
+    strategy = Class.new do
+      define_method(:run) do |args|
+       output
+      end
+    end
+    strategy.new
+  end
+
   test "shellOut returns the output from the strategy's 'run' method" do
     strategy = Class.new do
       def run(args)
         "output"
       end
     end
-
-    assert_equal "output", @instance.shellOut(strategy.new)
+    simulatorStrategy = mockClass("output")
+    assert_equal "output", @instance.shellOut(simulatorStrategy)
   end
 
-    test "shellOut returns the binary output from the strategy's 'run' method" do
-      bin = 0b000000000110111100000000011101010000000001110100000000000111000000000000011101010000000001110100
-
-    strategy = Class.new do
-      def run(args)
-        0b000000000110111100000000011101010000000001110100000000000111000000000000011101010000000001110100
-      end
-    end
-
-    assert_equal 0b000000000110111100000000011101010000000001110100000000000111000000000000011101010000000001110100, @instance.shellOut(strategy.new)
+  test "shellOut returns the binary output from the strategy's 'run' method" do
+    bin = 0b000000000110111100000000011101010000000001110100000000000111000000000000011101010000000001110100
+    assemblerStrategy = mockClass(bin)
+    assert_equal bin, @instance.shellOut(assemblerStrategy)
   end
 end
