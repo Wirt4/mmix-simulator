e6845721fd060cd1d6cd93a99a38e343e04cdb82
Wirt S. <34081557+Wirt4@users.noreply.github.com>
2026-04-12 20:24:32 -0500

    basic interface for assembling code

--- wasm/src/glue.c
+++ wasm/src/glue.c
@@ -1,2 +1,46 @@
 #include "glue.h"
 #include "mmix_interface.h"
+#include <stdint.h>
+
+/*
+ * allocates memory for the string buffer
+ * inputs: integer
+ * outputs: pointer to the memory
+ */
+uint8_t* allocate_input_buffer(int size){
+	return 0;
+} 
+
+/*
+ * frees memory pointed to by p
+ * inputs: uint_t pointer p
+ * outpus: void
+ */
+void free_input_buffer(char* p){}
+
+/*
+ * compiles string held in `input_buffer` to machine code results accessable from `binary_result`
+ * preconditions: string mmixal code is previously set via `allocate_input_buffer`
+ * postconditions: `get_binary_result_pointer` will return a pointer to the compiled binary and 
+ * `get_binary_result_size` will return the size in bytes of the result 
+ */
+void assemble_mmixal(){}
+
+/**
+ * returns a pointer to to the block of memory containing the machine code to be run on the mmix_sim
+ */
+int get_binary_result_pointer(){
+	return -1;
+}
+
+/**
+ * returns the size in bytes of the allocated block containing the binary result
+ */
+int get_binary_result_size(){
+	return -1;
+}
+
+/**
+ * de-allocates the memory pointed to by the argument p
+ */
+void free_binary_result(unsigned char* p){}
--- wasm/src/glue.h
+++ wasm/src/glue.h
@@ -1,3 +1,5 @@
+#include <stdint.h>
+
 #ifndef GLUE_H
 #define GLUE_H
 
@@ -7,4 +9,40 @@
  * Public API exported to WASM via Emscripten.
  */
 
+/**
+ * allocates memory for the string buffer
+ * inputs: integer
+ * outputs: pointer to the memory
+ */
+uint8_t* allocate_input_buffer(int size); 
+
+/*
+ * frees memory pointed to by p
+ * inputs: uint_t pointer p
+ * outpus: void
+ */
+void free_input_buffer(char* p);
+
+/*
+ * compiles string held in `input_buffer` to machine code results accessable from `binary_result`
+ * preconditions: string mmixal code is previously set via `allocate_input_buffer`
+ * postconditions: `get_binary_result_pointer` will return a pointer to the compiled binary and 
+ * `get_binary_result_size` will return the size in bytes of the result 
+ */
+void assemble_mmixal();
+
+/**
+ * returns a pointer to to the block of memory containing the machine code to be run on the mmix_sim
+ */
+int get_binary_result_pointer();
+
+/**
+ * returns the size in bytes of the allocated block containing the binary result
+ */
+int get_binary_result_size();
+
+/**
+ * de-allocates the memory pointed to by the argument p
+ */
+void free_binary_result(unsigned char* p);
 #endif /* GLUE_H */
