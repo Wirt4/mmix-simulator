d327535194920a48b2ef6ff0bb8faefbb63a899d
Wirt S. <34081557+Wirt4@users.noreply.github.com>
2026-05-05 14:54:30 -0500

    simplified layout

--- app/javascript/panel/panel_state_store.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface PanelState {
-  size: number | null
-  collapsed: boolean
-}
-
-export interface IPanelStateStore {
-  load(): PanelState
-  save(update: Partial<PanelState>): void
-}
