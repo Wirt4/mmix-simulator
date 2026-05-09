1f928a380513e72fa8d4763f38061a12c19de95d
Wirt S. <34081557+Wirt4@users.noreply.github.com>
2026-05-08 21:42:28 -0700

    Program Titles (#68)
    * red test
    
    * added title editing back to app
    
    * linter errors
    
    * DRY css

--- app/javascript/controllers/index.ts
+++ app/javascript/controllers/index.ts
@@ -1,5 +1,7 @@
 import { application } from "./application"
 import IDEFacadeController from "./ide_facade_controller"
+import InlineEditController from "./inline_edit_controller"
 import TextFormatController from "./text_format_controller"
 application.register("ide-facade", IDEFacadeController)
+application.register("inline-edit", InlineEditController)
 application.register("text-format", TextFormatController)
